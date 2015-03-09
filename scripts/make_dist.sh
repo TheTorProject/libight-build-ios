11992b462e93463c7ed53423a0f7b532676b0674
Simone Basso <bassosimone@gmail.com>
2015-03-09 21:48:42 +0100

    scripts/make_dist.sh: avoid hardcode dirnames

--- scripts/make_dist.sh
+++ scripts/make_dist.sh
@@ -1,21 +1,14 @@
 #!/bin/sh -e
 
 DESTDIR=dist
-DIRS=""
 STEMS="libevent libight libyaml-cpp"
-
-DIRS="$DIRS build/iPhoneSimulator/i386/lib"
-DIRS="$DIRS build/iPhoneSimulator/x86_64/lib"
-DIRS="$DIRS build/iPhoneOS/armv7/lib"
-DIRS="$DIRS build/iPhoneOS/armv7s/lib"
-DIRS="$DIRS build/iPhoneOS/arm64/lib"
 
 rm -rf dist/*
 install -d dist/lib
 for NAME in $STEMS; do
     for EXT in .a .dylib; do
         SOURCES=""
-        for DIR in $(echo $DIRS); do
+        for DIR in build/*/*/lib; do
             SOURCES="$SOURCES $DIR/$NAME$EXT"
         done
         lipo -create -output $DESTDIR/lib/$NAME$EXT $SOURCES
