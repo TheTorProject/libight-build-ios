33450c05cc316f1bc58e5ad68ad947f0b9dee947
Simone Basso <bassosimone@gmail.com>
2015-03-09 20:17:54 +0100

    Add script to compile single architecture

---
+++ scripts/build_arch.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -e
+
+if [ $# -ne 2 ]; then
+    echo "$0 platform arch" 1>&2
+    echo "Example: $0 iPhoneSimulator i386"
+    exit 1
+fi
+
+PLATFORM=$1
+ARCH=$2
+
+DEVELOPER=$(xcode-select -print-path)
+REPOROOT=$(pwd)
+MINIOSVERSION="6.1"
+SDKVERSION="8.1"
+
+export PATH="${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain/usr/bin/:${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/usr/bin/:${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain/usr/bin:${DEVELOPER}/usr/bin:${PATH}"
+
+export CC="/usr/bin/gcc -arch ${ARCH} -miphoneos-version-min=${MINIOSVERSION}"
+export CXX="/usr/bin/g++ -arch ${ARCH} -miphoneos-version-min=${MINIOSVERSION}"
+export CPPFLAGS="-isysroot ${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/SDKs/${PLATFORM}${SDKVERSION}.sdk"
+export CFLAGS="-isysroot ${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/SDKs/${PLATFORM}${SDKVERSION}.sdk"
+export CXXFLAGS="-isysroot ${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/SDKs/${PLATFORM}${SDKVERSION}.sdk"
+
+(
+    cd libight
+    autoreconf -i
+    ./configure --with-libevent=builtin \
+                --with-libyaml-cpp=builtin \
+                --with-libboost=builtin
+    make -j4
+    make install V=0 DESTDIR=$REPOROOT/build/${PLATFORM}/${ARCH}
+    make distclean
+)
