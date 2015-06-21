6fb3c5aacf0ceef15e033f00854c8963d96cad43
Simone Basso <bassosimone@gmail.com>
2015-06-21 23:10:49 +0200

    Create event2_pthreads framework
    This framework does not contain any event2 headers (they are all
    bundled in event2.framework) and only contains code adding libevent2
    support for using pthreads functionality.

--- scripts/build_frameworks.sh
+++ scripts/build_frameworks.sh
@@ -24,6 +24,13 @@ lipo -create -output event2.framework/event2 \
   build/iPhoneSimulator/i386/lib/libevent.a \
   build/iPhoneSimulator/x86_64/lib/libevent.a
 
+lipo -create -output event2_pthreads.framework/event2_pthreads \
+  build/iPhoneOS/arm64/lib/libevent_pthreads.a \
+  build/iPhoneOS/armv7s/lib/libevent_pthreads.a \
+  build/iPhoneOS/armv7/lib/libevent_pthreads.a \
+  build/iPhoneSimulator/i386/lib/libevent_pthreads.a \
+  build/iPhoneSimulator/x86_64/lib/libevent_pthreads.a
+
 lipo -create -output ight.framework/ight \
   build/iPhoneOS/arm64/lib/libight.a \
   build/iPhoneOS/armv7s/lib/libight.a \
