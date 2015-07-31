54f9a0c17dddecf2cfdde539500241cef3763d3f
Simone Basso <bassosimone@gmail.com>
2015-07-31 09:14:26 +0200

    SDKVERSION: 8.3 -> 8.4
    Requested by @lorenzoPrimi.

--- scripts/build_arch.sh
+++ scripts/build_arch.sh
@@ -16,7 +16,7 @@ fi
 DEVELOPER=$(xcode-select -print-path)
 REPOROOT=$(pwd)
 MINIOSVERSION="6.1"
-SDKVERSION="8.3"
+SDKVERSION="8.4"
 
 export PATH="${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain/usr/bin/:${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/usr/bin/:${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain/usr/bin:${DEVELOPER}/usr/bin:${PATH}"
 
